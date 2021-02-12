48b9e40b298f79f00f53ee4cefd998d7b996016f
pecalee <studiopeca.lee@gmail.com>
2021-02-12 20:27:17 +0900

    followMouse

--- day_1/sketch.js
+++ day_1/sketch.js
@@ -1,29 +1,34 @@
+const fr = 60;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  background(random(0, 100), random(0, 100), random(0, 100));
+  background(0);
   noFill();
+  frameRate(fr);
 }
 
 function windowResized() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  background(random(0, 100), random(0, 100), random(0, 100));
+  background(0);
   noFill();
+  frameRate(fr);
 }
 
 let i = 0;
+let drawBox;
 
 function draw() {
-  strokeWeight(random(1, 3));
-  stroke(random(100, 255), random(100, 255), random(100, 255));
+  translate(mouseX - windowWidth / 2, mouseY - windowHeight / 2, 0);
   if (i === 5) {
     i = 0;
-    background(random(0, 30), random(0, 30), random(0, 30));
+    background(0);
   } else {
     ++i;
   }
-  rotateX(frameCount * 0.05);
-  rotateY(frameCount * 0.05);
-  //translate(mouseX - 200, mouseY - 200);
-  const randomBox = random(0, windowHeight / 2);
-  box(randomBox, randomBox, randomBox);
+  strokeWeight(random(0.5, 1));
+  stroke(random(100, 255), random(100, 255), random(100, 255));
+  rotateX(frameCount * 1.2);
+  rotateY(frameCount * 1.2);
+  const randomBox = random(0, windowHeight / 10);
+  drawBox = box(randomBox, randomBox, randomBox);
 }
