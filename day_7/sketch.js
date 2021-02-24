8cda71c1758a493ad79017d163f526f3eb38351d
pecalee <studiopeca.lee@gmail.com>
2021-02-24 23:32:07 +0900

    day7

--- day_7/sketch.js
+++ day_7/sketch.js
@@ -1,3 +1,5 @@
+let line;
+
 function setup() {
   let canvasOne = createCanvas(windowWidth, windowHeight);
   canvasOne.parent("processingContainer");
@@ -12,7 +14,17 @@ function windowResized() {
 
 function draw() {
   stroke(0);
-  strokeWeight(2);
-  noFill();
-  circle(mouseX, mouseY, 200);
+  strokeWeight(4);
+  fill(255);
+  if ((line === undefined) | (line < 0)) {
+    line = width * 2;
+    if (line > 0) {
+      line = line - 30;
+    }
+  } else {
+    if (line > 0) {
+      line = line - 30;
+    }
+  }
+  circle(mouseX, mouseY, line);
 }
