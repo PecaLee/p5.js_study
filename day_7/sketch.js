bb296bc349890e75c2a7421caaee476ade11ab85
pecalee <studiopeca.lee@gmail.com>
2021-02-25 23:05:20 +0900

    어떻게하면더 자연스럽게 표현할 수 있을까

--- day_7/sketch.js
+++ day_7/sketch.js
@@ -1,7 +1,8 @@
 //마우스가 움직였는지 체크해서 움직였으면 마우스의 가운데부터 원을 중첩해서 그려나감.
 
 let radiusC,
-  cicleWidth = 0;
+  cicleWidth = 0,
+  cicleWidthT = 0;
 
 function setup() {
   let canvasOne = createCanvas(windowWidth, windowHeight);
@@ -22,7 +23,24 @@ function draw() {
   if (radiusC === undefined) {
     radiusC = width * 2;
   }
-  for (i = 0; i < radiusC; i = i + 40) {
-    circle(mouseX, mouseY, i);
+  /**for (i = 0; i < radiusC; i = i + 40) {
+    for (j = i; j > 0; j = j - 40) {
+      circle(mouseX, mouseY, j);
+    }
+  }*/
+  if (cicleWidth < radiusC) {
+    cicleWidth = cicleWidth + 40;
+    if (cicleWidth > 0) {
+      cicleWidthT = cicleWidth;
+      for (j = cicleWidthT; j > 0; j = j - 40) {
+        circle(mouseX, mouseY, j);
+      }
+      /** 
+      cicleWidthT = cicleWidthT - 40;
+      circle(mouseX, mouseY, cicleWidth);
+      */
+    }
+  } else {
+    cicleWidth = 0;
   }
 }
